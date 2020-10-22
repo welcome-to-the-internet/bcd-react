7f7fd182964b38885177994ff26c4c4d36d82031
Kyle O'Brien <obrienk@webbhost.net>
2020-10-22 17:12:19 -0500

    Update index.js
    modify title

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-const title = 'BigCom.dev';
+const title = 'Big Community.dev';
 ReactDOM.render(<App title={title} />, document.getElementById('root'));
 if (typeof window !== 'undefined') {
   React.render(<MainWrapper />, document.getElementById('root'));
